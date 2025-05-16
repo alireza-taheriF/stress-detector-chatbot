1429fa6289a8d32d7da0a8d36cb4bc519758e208
alireza-taheriF <alirezataherifakhr@icloud.com>
2025-05-16 14:39:23 +0330

    Update combined.py

--- combined.py
+++ combined.py
@@ -1,29 +1,29 @@
 import pandas as pd
 from sklearn.utils import shuffle
 
-# 1. بارگذاری دیتافریم‌های تمیزشده
+#1. Loading cleaned data frames
 df_suicide = pd.read_csv("suicide_detection_cleaned.csv")
 df_tweets  = pd.read_csv("training_cleaned.csv")
 
-# 2. برچسب‌گذاری دیتافریم Suicide
+# 2. Suicide Dataframe Tagging
 df_suicide['label'] = 1
 
-# 3. فیلتر و برچسب‌گذاری دیتافریم توییت‌ها
+# 3. Filtering and Tagging Tweet Dataframe
 df_t = df_tweets[df_tweets['polarity'].isin([0,4])].copy()
 df_t['label'] = df_t['polarity'].apply(lambda p: 1 if p==0 else 0)
 
-# 4–5. انتخاب فقط ستون‌های لازم
+# 4–5. Select only the necessary columns
 df_suicide = df_suicide[['clean_text','label']]
 df_t      = df_t[['clean_text','label']]
 
-# 6. ادغام
+#6. Integration
 df_combined = pd.concat([df_suicide, df_t], ignore_index=True)
 
 # 7. Shuffle
 df_combined = shuffle(df_combined, random_state=42)
 
-# 8. ذخیره
+#8. Save
 df_combined.to_csv("combined_dataset.csv", index=False)
 
-# 9. نمایش نمونه
+#9. Sample display
 print(df_combined.head())
