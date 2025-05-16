db9c788aac58e85527ca07482fa2269eaaa99b31
Alireza Taheri <alireza@example.com>
2025-05-16 14:35:38 +0330

    Add MIT license file

--- tests/test-1.py
+++
@@ -1,6 +0,0 @@
-name = input("Hi, enter your name: ")
-
-if 'm' in name or 'M' in name:
-    print(f"Hey {name}, your name is the wining card!")
-else:
-    print(f"ops {name} unfortuantly your name does not suit our goal! CUL")
